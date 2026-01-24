6c10e3ee11ce06f6f2c8239d174f050ffa45355c
Cat Core <arthur.koalas@free.fr>
2026-01-24 23:38:11 +0100

    Fix version comparison

--- src/main/java/net/ornithemc/keratin/api/manifest/VersionDetails.java
+++ src/main/java/net/ornithemc/keratin/api/manifest/VersionDetails.java
@@ -1,7 +1,5 @@
 package net.ornithemc.keratin.api.manifest;
 
-import com.vdurmont.semver4j.Semver;
-
 import java.util.Objects;
 
 public record VersionDetails(String id, String normalizedVersion, boolean client, boolean server, boolean sharedMappings, Downloads downloads) implements Comparable<VersionDetails> {
@@ -14,17 +12,14 @@ public record VersionDetails(String id, String normalizedVersion, boolean client
 
 	@Override
 	public int compareTo(VersionDetails o) {
-        return new Semver(Objects.requireNonNullElseGet(normalizedVersion, () -> normalize(id))).compareTo(new Semver(Objects.requireNonNullElseGet(o.normalizedVersion, () -> normalize(o.id))));
+        return normalize(Objects.requireNonNullElse(normalizedVersion, id)).compareTo(normalize(Objects.requireNonNullElse(o.normalizedVersion, o.id)));
 	}
 
 	public int compareTo(String o) {
-        return new Semver(Objects.requireNonNullElseGet(normalizedVersion, () -> normalize(id))).compareTo(new Semver(normalize(o)));
+        return normalize(Objects.requireNonNullElse(normalizedVersion, id)).compareTo(normalize(o));
 	}
 
-	public static String normalize(String version) {
-		if (Objects.equals(version, "15w14a")) return "1.8.4-april.fools";
-		if (version.lastIndexOf('.') == 1) return version + ".0";
-
-		return version;
+	public static net.fabricmc.loader.api.SemanticVersion normalize(String version) {
+		return MCVersionLookup.parse(version);
 	}
 }
